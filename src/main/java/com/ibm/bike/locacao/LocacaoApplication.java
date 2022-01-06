01aa49b9d77e91da119b11185c55e29a4df73a20
byancacmaia@gmail.com <byancamaia@ibm.com>
2022-01-06 20:55:56 -0300

    fix: testing

--- src/main/java/com/ibm/bike/locacao/LocacaoApplication.java
+++ src/main/java/com/ibm/bike/locacao/LocacaoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LocacaoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Teste de commit");
+		System.out.println("Teste de commit 2");
 		SpringApplication.run(LocacaoApplication.class, args);
 	}
 
